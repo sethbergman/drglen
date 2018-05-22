4f4eb3ff7d37204eb1237cea1772b38ea4ba0501
Seth Bergman <seth.atxwebs@gmail.com>
2018-05-22 05:22:10 -0500

    deploy

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-app.engine("html", require("ejs").renderFile);
-app.set("/views", express.static(__dirname + "/views"));
-app.set("view engine", "html");
-app.use("/assets", express.static(__dirname + "/assets"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.engine('html', require('ejs').renderFile)
+app.set('/views', express.static(__dirname + '/views'))
+app.set('view engine', 'html')
+app.use('/assets', express.static(__dirname + '/assets'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", function(request, response) {
-  response.render("index");
-});
+app.get('/', function(request, response) {
+  response.render('index')
+})
 
-app.get("/2", function(request, response) {
-  response.render("2");
-});
+app.get('/2', function(request, response) {
+  response.render('2')
+})
+
+app.get('/3', function(request, response) {
+  response.render('3')
+})
 
 const server = app.listen(process.env.PORT || 5000, function() {
   console.info('Server running at http://0.0.0.0:' + server.address().port)
-});
+})
