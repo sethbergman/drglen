65db3bac541f4d21b0b3c9445c132a0d8d183a48
Seth Bergman <seth.atxwebs@gmail.com>
2019-11-08 20:59:00 -0600

    Added Day-1-Paperwork PDF

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 const app = express();
 
 app.engine("html", require("ejs").renderFile);
 app.set("/views", express.static(__dirname + "/views"));
 app.set("view engine", "html");
 app.use("/assets", express.static(__dirname + "/assets"));
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", function(request, response) {
-  response.render("index2");
+app.get("/", (req, res) => {
+  res.render("index2");
+});
+
+app.get("/day-1-paperwork", (req, res) => {
+  var filePath = "/views/day-1-paperwork.pdf";
+  fs.readFile(__dirname + filePath, function(err, data) {
+    res.contentType("application/pdf");
+    res.send(data);
+  });
 });
 
 const server = app.listen(process.env.PORT || 5000, function() {
